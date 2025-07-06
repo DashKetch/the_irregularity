bb1b3fbbd154966a74cef07737ed44ff1ed6ac0c
DashKetch <Nate@DESKTOP-6K5PDH0>
2025-07-06 13:44:30 -0700

    updated/added files to mod version 1.0.1

--- src/main/java/net/mcreator/theirregularity/procedures/IrregularSwordRightclickedProcedure.java
+++ src/main/java/net/mcreator/theirregularity/procedures/IrregularSwordRightclickedProcedure.java
@@ -2,6 +2,7 @@ package net.mcreator.theirregularity.procedures;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.Difficulty;
 import net.minecraft.server.level.ServerLevel;
@@ -11,7 +12,9 @@ public class IrregularSwordRightclickedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		if (!(world.getDifficulty() == Difficulty.PEACEFUL)) {
 			if (world instanceof ServerLevel _level)
-				FallingBlockEntity.fall(_level, BlockPos.containing(x, y + 30, z), Blocks.ANVIL.defaultBlockState());
+				FallingBlockEntity.fall(_level, BlockPos.containing(x, y + 30, z), Blocks.CRIMSON_HYPHAE.defaultBlockState());
+			if (world instanceof Level _level && !_level.isClientSide())
+				_level.explode(null, x, y, z, 12, Level.ExplosionInteraction.BLOCK);
 		}
 	}
 }
